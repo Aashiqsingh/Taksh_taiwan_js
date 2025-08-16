f534875f10335b18781d51a36489758b5edadfa8
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-16 10:21:19 +0530

    drop down

---
+++ dropDown/dropDown.js
@@ -0,0 +1,17 @@
+const changeColor = ()=>{
+    let colors = document.getElementById("colors");
+
+    console.log(colors.value);
+    var mydiv = document.getElementById("mydiv");
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = colors.value;
+    mydiv.style.margin = "100px 400px"
+    
+}
+
+const changeRadius = ()=>{
+    let radius = document.getElementById("radius");
+
+    mydiv.style.borderRadius = radius.value;
+}
